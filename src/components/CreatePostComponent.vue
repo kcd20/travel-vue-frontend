7738c9cb76a8db90f067ecffe0747f20b739040d
Kok Cheng Da <kokchengda@Koks-MacBook-Pro.local>
2025-12-30 23:49:33 +0800

    add editor

---
+++ src/components/CreatePostComponent.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TextEditorComponent from './TextEditorComponent.vue'
+
+const title = ref('')
+const content = ref('')
+
+function handleSubmit() {
+  const payload = {
+    title: title.value,
+    description: content.value,
+  }
+  console.log('Form submitted:', payload)
+}
+</script>
+
+<template>
+  <form @submit.prevent="handleSubmit" class="form">
+    <div class="title-container">
+      <label for="title">Title:</label>
+      <input
+        class="title"
+        v-model="title"
+        type="text"
+        placeholder="Enter a title"
+      />
+    </div>
+
+    <div class="title-container">
+      <label class="block font-medium mb-1">Description:</label>
+      <TextEditorComponent v-model="content" />
+    </div>
+
+    <button type="submit" class="submit">
+      Submit
+    </button>
+  </form>
+</template>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+}
+.title-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+}
+.title {
+  height: 1.5rem;
+}
+.submit {
+  width: min-content;
+}
+</style>
---
+++ src/components/TextEditorComponent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+const content = defineModel<string>()
+</script>
+
+<template>
+  <div>
+    <QuillEditor v-model:content="content" theme="snow" content-type="html" />
+  </div>
+</template>
+
+<style scoped>
+:deep .ql-editor {
+  min-height: 10rem;
+}
+</style>
